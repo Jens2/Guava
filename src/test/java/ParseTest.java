947ff516af0acd85ad9311682ec139d6ba0faa56
Jens2 <j.vdmeer10@hotmail.com>
2016-06-14 12:08:04 +0200

    Initial commit

---
+++ src/test/java/ParseTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * Created by Jens on 14-6-2016.
+ *
+ */
+public class ParseTest {
+
+    /** Test the parsing phase of the compiler. */
+    @Test
+    public void test() {
+
+    }
+}
