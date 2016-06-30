57309ddfc7b0ea511c15aa641c4e471546916776
Dion Ikink <ikinkdion@gmail.com>
2016-06-30 16:30:27 +0200

    Added JavaDoc (partially)

--- src/main/java/elaboration/SymbolTable.java
+++ src/main/java/elaboration/SymbolTable.java
@@ -1,11 +1,29 @@
 package elaboration;
 
 public interface SymbolTable {
-	public abstract void openScope();
+    /**
+     * Opens a new scope
+     */
+	void openScope();
 
-	public abstract void closeScope();
+    /**
+     * Closes the deepest scope
+     */
+	void closeScope();
 
-	public abstract boolean add(String id, Type type, boolean global);
+    /**
+     * Adds a variable to the deepest scope
+     * @param id the name of the variable
+     * @param type the type of the variable
+     * @param global whether the variable is defined as global or not
+     * @return <tt>false</tt> if the variable was already declared in the current scope
+     */
+	boolean add(String id, Type type, boolean global);
 
-	public abstract boolean contains(String id);
+    /**
+     * Checks whether the given variable is already in any of the scopes
+     * @param id the name of the variable
+     * @return <tt>true</tt> if the given variable is already in the table
+     */
+	boolean contains(String id);
 }
