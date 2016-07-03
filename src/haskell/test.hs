0c35fe999fda4b011c1e972231b076d0a9a7d470
Dion Ikink <ikinkdion@gmail.com>
2016-07-03 17:45:03 +0200

    Added integer division and power to Sprockell, removed modulo

--- src/haskell/test.hs
+++ src/haskell/test.hs
@@ -12,9 +12,14 @@ a = 1
 b = 2
 
 program :: [Instruction]
-program = [ LoadConst 5 regA
-          , LoadConst 2 regB
-          , Compute Pow regB regA regC
+program = [ LoadConst 97 regA
+          , Store regA (DirAddr a)
+          , Load (DirAddr a) regA
+          , LoadConst 98 regB
+          , Compute Gt regA regB regC
+          , Store regC (DirAddr b)
+          , ReadInstr (DirAddr 0)
+          , Receive regC
           , EndProg ]
 
 testProgram = sysTest [program]
